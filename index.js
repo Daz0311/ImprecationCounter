b94395878d8bde131c82239fcc8491f250601c19
DT-08\Admin <dario.tarasco@remira.com>
2024-08-12 18:04:08 +0200

    recreated an imprecation counter, changed background, fixed buttons, added reset button

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let saveEl = document.getElementById("save-el")
 let countNum = document.getElementById("count-num")
+let decrementBtn = document.getElementById("decrement-btn")
+let incrementBtn = document.getElementById("increment-btn")
+let saveBtn = document.getElementById("save-btn")
+let resetButton = document.getElementById("reset-btn")
+
 let count = 0
 
-console.log(countNum)
+
+incrementBtn.addEventListener("click", increment);
+decrementBtn.addEventListener("click", decrement);
+saveBtn.addEventListener("click", save);
+resetButton.addEventListener("click", reset);
+
 
 
 function increment() {
@@ -11,11 +21,23 @@ function increment() {
 }
 
 function decrement() {
+    if (count > 0) {
     count -= 1
     countNum.innerText = count
+    }
 }
 
 function save() {
     let countStr = count + " - "
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr
+    count = 0;
+    countNum.textContent = 0;
+}
+
+function reset() {
+    console.log("pressed")
+    count= 0;
+    countNum.innerText = 0;
+    saveEl.innerText = "Previous imprecations: "
+
 }
